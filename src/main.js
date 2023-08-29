9057f446a012a4807c9cad7e5b3016124ae0dad8
dreamfly <420443292@qq.com>
2023-08-29 18:49:36 -0500

    Update main.js

--- src/main.js
+++ src/main.js
@@ -85,6 +85,3 @@ app.on('window-all-closed', function () {
     app.quit()
   }
 })
-
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
